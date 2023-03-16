f18440ee34dfdeb1849972ca2fa240e1857df250
Manuel Mares <manuels@nmsu.edu>
2023-03-15 20:25:21 -0600

    deleted extra button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Link } from "react-router-dom";
-<Link to="about">Click to view our about page</Link>
 
 export class Button extends Component{
     render(){
